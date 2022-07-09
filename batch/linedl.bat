2175b852e7eea05d48ca46348b9a5f6c68ea1e25
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-07-09 22:15:57 +0800

    Update linedl.bat

--- batch/linedl.bat
+++ batch/linedl.bat
@@ -1,4 +1,6 @@
-set name=%2.png
-curl %1 -o %name%
+IF [%1] == [] (set /p url="Enter URL: ") else (set url=%1)
+IF [%2] == [] (set /p name="Enter FileName: ") else (set name=%2)
+set name=%name%.png
+curl %url% -o %name%
 start /wait AnimatedPngToGif.bat %name%
 DEL %name%
