8388479118eb81a721b8c6a72483e57fd0eac45d
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-07-09 15:59:11 +0800

    Update AnimatedPngToGif.bat

--- batch/AnimatedPngToGif.bat
+++ batch/AnimatedPngToGif.bat
@@ -1,5 +1,9 @@
 set filename=%1
 set name=%filename:.png=%
+set stickerFolder=C:\Users\%username%\Pictures\stickers\
+
+
 ffmpeg -i %filename% -vf palettegen=reserve_transparent=1 custom_transparant_palette_for_convert12873640192471230974.png
-ffmpeg -i %filename% -i custom_transparant_palette_for_convert12873640192471230974.png -lavfi paletteuse=alpha_threshold=128 -gifflags -offsetting -r 30 %name%.gif
+ffmpeg -i %filename% -i custom_transparant_palette_for_convert12873640192471230974.png -lavfi paletteuse=alpha_threshold=128 -gifflags -offsetting -r 30 %stickerFolder%%name%.gif
 DEL custom_transparant_palette_for_convert12873640192471230974.png
+exit
