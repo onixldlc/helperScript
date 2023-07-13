26ea88282b4d4c8d64a85a249b48d07c0e8c8882
onixldlc <81515667+onixldlc@users.noreply.github.com>
2023-07-14 03:26:50 +0700

    Create sha256sum.bat

---
+++ batch/sha256sum.bat
@@ -0,0 +1,10 @@
+@echo off
+echo:
+for %%x in (%*) do (
+  for /F "tokens=*" %%n in ('certutil -hashfile "%%~x" SHA256') do (
+    set out = %%n
+    echo %%n | >nul findstr /i /r /c:"^SHA256 hash of.*" && ( echo %%n )
+    echo %%n | >nul findstr /i /r /c:"^[abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890].*" && ( echo %%n )
+    echo %%n | >nul findstr /i /r /c:"^CertUtil:.*" && ( echo: )
+  )
+)
