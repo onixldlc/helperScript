c107c8bd9d0b986aa130d51d2cd4bead00d2d175
onixldlc <81515667+onixldlc@users.noreply.github.com>
2023-07-14 03:24:54 +0700

    Update md5sum.bat

--- batch/md5sum.bat
+++ batch/md5sum.bat
@@ -3,7 +3,7 @@ for %%x in (%*) do (
   for /F "tokens=*" %%n in ('certutil -hashfile "%%~x" MD5') do (
     set out = %%n
     echo %%n | >nul findstr /i /r /c:"^MD5 hash of.*" && ( echo %%n )
-    echo %%n | >nul findstr /i /r /c:"[abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890].*" && ( echo %%n )
+    echo %%n | >nul findstr /i /r /c:"^[abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890][abcdef1234567890].*" && ( echo %%n )
     echo %%n | >nul findstr /i /r /c:"^CertUtil:.*" && ( echo: )
   )
 )
